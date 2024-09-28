dcb559ec286530b57b94f295f41574baabbd714f
Feather <jsgj20@yahoo.com>
2024-09-28 18:32:31 +0800

    Add python data types

---
+++ dataTypes.py
@@ -0,0 +1,17 @@
+#String
+name = "Clyd"
+school = "University of the Philippines Quezon City"
+
+#Integer
+age = 17
+
+#Float
+GPA = 97.5
+
+#Boolean
+isBoy = True
+
+
+print(f"My name is {name} {age} years old and I am stuying at {school}")
+print(f"My GPA is {GPA} and am I a boy? {isBoy}")
+
